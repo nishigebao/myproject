8f51df9259651f180b362db4f0b8d67f85f5df81
silence <2521010451@qq.com>
2018-10-06 22:41:49 +0800

    初始化提交

---
+++ ssm_project/src/main/java/com/hr/SsmProjectApplication.java
@@ -0,0 +1,12 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmProjectApplication.class, args);
+	}
+}
